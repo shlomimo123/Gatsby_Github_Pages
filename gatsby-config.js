a5e2ccfdfb9515a635bccd81003c197ff30d1052
Shlomi Moshe <shlomimo@matrix.co.il>
2018-03-22 09:01:46 +0200

    adding netlifycms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  pathPrefix: '/Gatsby_Github_Pages',
+  // pathPrefix: '../..',
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
   plugins: [
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     'gatsby-plugin-sass',
